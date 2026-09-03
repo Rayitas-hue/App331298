3ad17ce4fb581d883f413e81624c7bd69ba9074c
Rayitas-hue <miguelangelbedoyaorjuela@gmail.com>
2026-09-03 16:57:26 -0500

    Actualizacion y correcciones

---
+++ web_frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <footer className="border-t bg-white py-3 text-center text-sm text-gray-500">
+            API EDUCATIVA &copy; 2026 ● versión 1.0.0
+        </footer>
+    );
+}
--- web_frontend/src/pages/Dashboard/Dashboard.tsx
+++ web_frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,18 +1,13 @@
+import DashboardLayout from "../../layouts/DasboardLayout";
+
 export default function Dashboard()
 {
-    return (<div className="min-h-screen flex items-center justify-center bg-slate-100">
-    <div className="rounded-xl bg-white shadow-xl p-10">
-        <h1 className="text-4xl font-bold text-blue-600">
-        API EDUCATIVA
-        </h1>
-
-        <p className="mt-3 text-gray-600">
-        React + Vite + Tailwind CSS v4 funcionando correctamente 🚀
-        </p>
-
-        <button className="mt-6 rounded-lg bg-blue-600 px-5 py-2 text-white hover:bg-blue-700">
-        Comenzar
-        </button>
-    </div>
-    </div>);
+    return (
+        <DashboardLayout>
+            <h1 className="mb-6 text-3xl font-bold">Dasboard</h1>
+            <p>
+                Bienvenido al sistema API Educativa
+            </p>
+        </DashboardLayout>
+    );
 }
